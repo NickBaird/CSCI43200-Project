a14974d37ce43c7ec7c88d84a99a4d0dfd4a0bad
NickBaird <84865088+NickBaird@users.noreply.github.com>
2024-02-23 22:03:48 -0500

    init commit

---
+++ js.js
@@ -0,0 +1,364 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCvGGb0NWirKcKX9P_krRogy5BdN1XUxww",
+    authDomain: "e2ee-messengers.firebaseapp.com",
+    projectId: "e2ee-messengers",
+    storageBucket: "e2ee-messengers.appspot.com",
+    messagingSenderId: "979623333154",
+    appId: "1:979623333154:web:91a56de139f27fb365297b"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const database = firebase.database();
+
+const dbName = 'testdatabase3';
+const dbVersion = 2;
+
+var public = null;
+var display = null;
+var map = {};
+
+var loaded = null;
+var otherdisplay = null;
+var received_messages = [];
+var sent_messages = [];
+
+
+
+var unsubscribe = firebase.auth().onAuthStateChanged(function(user) {
+    if(auth.currentUser != null) {
+        initialize();
+        unsubscribe();
+    }
+});
+
+
+
+
+
+function register() {
+    email = document.getElementById('register-email');
+    password = document.getElementById('register-password');
+    display = document.getElementById('register-display');
+
+    auth.createUserWithEmailAndPassword(email.value, password.value).then((userCredential) => {
+        var user = userCredential;
+        var keys = sodium.crypto_kx_keypair();
+        var userData = {
+            display : display.value,
+            public : sodium.to_hex(keys.publicKey)
+        };
+        database.ref('/users/' + auth.currentUser.uid).set(userData);
+        localStorage.setItem("private", sodium.to_hex(keys.privateKey));
+
+        const indexed = window.indexedDB.open(dbName, dbVersion);
+
+        indexed.onupgradeneeded = function(event) {
+
+            console.log("UPGRADE")
+            const db = event.target.result;
+            // Check if the object store 'keys' exists
+            if (!db.objectStoreNames.contains('keys')) {
+                const objectStore = db.createObjectStore('keys', { keyPath: 'uid' });
+            }
+        };
+
+        indexed.onsuccess = function(event) {
+            console.log("SUCCESS");
+            var key = get_private_local()
+            const db = event.target.result;
+
+            var transaction = db.transaction(['keys'], 'readwrite');
+            var objectStore = transaction.objectStore('keys');
+            var keyData = { uid: auth.currentUser.uid, privateKey: key };
+            var request = objectStore.put(keyData);
+
+            request.onsuccess = function(event) {
+                console.log('Private key stored successfully');
+            };
+
+            request.onerror = function(event) {
+                console.error('Error storing private key');
+            };
+        };      
+    });
+}
+
+function login() {
+    email = document.getElementById('login-email');
+    password = document.getElementById('login-password');
+
+    auth.signInWithEmailAndPassword(email.value, password.value).then((userCredential) => {
+        console.log(userCredential);
+    });
+
+}
+
+
+function initialize() {
+    console.log("INIT");
+    database.ref("/users/" + auth.currentUser.uid + "/public").get().then((pub) => {
+        public = pub.val();
+        database.ref("/users/" + auth.currentUser.uid + "/display").get().then((dis) => {
+            display = dis.val();
+            document.getElementById('user-container').innerHTML = "<h1>Welcome " + display + "!</h1> <br><p>User ID: " + public + "</p>";
+        });
+    });
+
+    database.ref("/users/" + auth.currentUser.uid + "/conversations").on('child_added', (sent) => {
+        add_to_map(sent.key)
+    }); 
+}
+
+async function add_to_map(uid) {
+    if(!map.hasOwnProperty(uid)) {
+        const otherPublic = (await database.ref("/users/" + uid + "/public").get()).val();
+        const otherDisplay = (await database.ref("/users/" + uid + "/display").get()).val();
+        const privateKey = await get_private();
+        const clientkeys = sodium.crypto_kx_client_session_keys(sodium.from_hex(public), sodium.from_hex(privateKey), sodium.from_hex(otherPublic));
+        const serverkeys = sodium.crypto_kx_server_session_keys(sodium.from_hex(public), sodium.from_hex(privateKey), sodium.from_hex(otherPublic));
+        map[uid] = {
+            hex : otherPublic,
+            display : otherDisplay,
+            crx : clientkeys.sharedRx,
+            ctx : clientkeys.sharedTx,
+            srx : serverkeys.sharedRx,
+            stx : serverkeys.sharedTx
+        };
+        update_conversations();
+    }
+}
+
+
+async function send_message(uid, message) {
+    await add_to_map(uid);
+    nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES);
+    encrypted = sodium.crypto_secretbox_easy(message, nonce, map[uid].ctx);
+
+    payload = {
+        timestamp : new Date().getTime(),
+        payload : sodium.to_hex(encrypted),
+        nonce : sodium.to_hex(nonce)
+    };
+
+    database.ref("/users/" + uid + "/messages/" + auth.currentUser.uid).push(payload);
+    database.ref("/users/" + auth.currentUser.uid + "/conversations/" + uid).set(1);
+    database.ref("/users/" + uid + "/conversations/" + auth.currentUser.uid).set(1);
+}
+
+function send_image(uid, image) {
+
+}
+
+async function get_received_messages(uid) {
+    received_messages = [];
+    return database.ref("/users/" + auth.currentUser.uid + "/messages/" + uid).on('child_added', (message) => {
+        console.log(message.val());
+        nonce = sodium.from_hex(message.child('nonce').val());
+        payload = sodium.from_hex(message.child('payload').val());
+        timestamp = message.child('timestamp').val();
+
+        decrypted = sodium.crypto_secretbox_open_easy(payload, nonce, map[uid].srx);
+
+        output = {
+            message: sodium.to_string(decrypted),
+            timestamp: timestamp
+        }
+        received_messages.push(output);
+        update_messages();
+    });
+}
+
+
+
+async function get_sent_messages(uid) {
+    sent_messages = [];
+    return database.ref("/users/" + uid + "/messages/" + auth.currentUser.uid).on('child_added', (message) => {
+        nonce = sodium.from_hex(message.child('nonce').val());
+        payload = sodium.from_hex(message.child('payload').val());
+        timestamp = message.child('timestamp').val();
+
+        decrypted = sodium.crypto_secretbox_open_easy(payload, nonce, map[uid].ctx);
+
+        output = {
+            message: sodium.to_string(decrypted),
+            timestamp: timestamp
+        }
+        sent_messages.push(output);
+        update_messages();
+    });
+}
+
+function new_conversation() {
+    unload_conversation();
+    var uid = document.getElementById('new-conversation-uid').value;
+    var message = document.getElementById('new-conversation-message').value;
+    console.log(uid + " " + message);
+    send_message(uid, message)
+    load_conversation(uid);
+}
+
+function load_conversation(uid) {
+    loaded = uid;
+    otherdisplay = map[uid].display;
+    get_received_messages(uid);
+    get_sent_messages(uid);
+
+    var container = document.getElementById('message-container');
+    container.innerHTML = "<input type=\"text\" id=\"message-box\"><button onclick=\"send_message(\'" + uid+ "\', " + "document.getElementById('message-box').value);\">Send</button>";
+}
+
+function unload_conversation() {
+    if(loaded != null) {
+        database.ref("/users/" + loaded + "/messages/" + auth.currentUser.uid).off();
+        database.ref("/users/" + auth.currentUser.uid + "/messages/" + loaded).off();
+        loaded = null;
+        otherdisplay = null;
+        received_messages = [];
+        sent_messages = [];
+        update_messages();
+        document.getElementById('message-container').innerHTML = "";
+    }
+}
+
+function update_messages() {
+    var container = document.getElementById('messages-container');
+    container.innerHTML = "";
+    var rcount = 0, scount = 0;
+    while(true) {
+        if (rcount < received_messages.length)
+            if(scount < sent_messages.length)
+                if (rcount < received_messages.length && received_messages[rcount].timestamp <= sent_messages[scount].timestamp){
+                    container.innerHTML += "<div class=\"other\"><h2>" + otherdisplay + ":  <h2><h3>" + received_messages[rcount].message + "</h3><br></div>"
+                    rcount++;
+                } else {
+                    container.innerHTML += "<div class=\"you\"><h2>You:  <h2><h3>" + sent_messages[scount].message + "</h3><br></div>"
+                    scount++;
+                }
+            else {
+                container.innerHTML += "<div class=\"other\"><h2>" + otherdisplay + ":  <h2><h3>" + received_messages[rcount].message + "</h3><br></div>"
+                rcount++;
+            }
+        else {
+            if(scount < sent_messages.length) {
+                container.innerHTML += "<div class=\"you\"><h2>You:  <h2><h3>" + sent_messages[scount].message + "</h3><br></div>"
+                scount++;
+            } else break;
+        }
+    }
+}
+
+function update_conversations() {
+    var container = document.getElementById('conversations-container');
+    container.innerHTML = "";
+    Object.keys(map).forEach((key) => {
+      container.innerHTML += "<div class=\"conversation\" onclick=\"load_conversation(\'" + key + "\')\"><h3>" + map[key].display + "</h3><br>" + key + "</div> <br>"
+    });
+}
+
+
+
+
+async function get_private() {
+    return new Promise((resolve, reject) => {
+        const indexed = window.indexedDB.open(dbName, dbVersion);
+
+        indexed.onsuccess = function(event) {
+
+            const db = event.target.result;
+            if (db.objectStoreNames.contains('keys')) {
+                var transaction = db.transaction(['keys'], 'readonly');
+                var objectStore = transaction.objectStore('keys');
+                var request = objectStore.get(auth.currentUser.uid);
+
+                request.onsuccess = function(event) {
+                    const keyData = event.target.result;
+                    if (keyData) {
+                        console.log('Private key retrieved successfully:', keyData.privateKey);
+                        resolve(keyData.privateKey);
+                    } else {
+                        console.error('Private key not found');
+                        reject(new Error('Private key not found'));
+                    }
+                };
+
+                request.onerror = function(event) {
+                    console.error('Error retrieving private key');
+                    reject(event.target.error);
+                };  
+            } else {
+                console.error('Private key not found!');
+                reject(new Error('Private key not found'));
+            }
+        };
+    });
+}
+
+
+
+
+
+// OLD FUNCTIONS
+function get_private_local() {
+    return localStorage.getItem("private");
+}
+
+function transfer_private() {
+    const indexed = window.indexedDB.open(dbName, dbVersion);
+
+    indexed.onupgradeneeded = function(event) {
+        const db = event.target.result;
+        // Check if the object store 'keys' exists
+        if (!db.objectStoreNames.contains('keys')) {
+            const objectStore = db.createObjectStore('keys', { keyPath: 'uid' });
+        }
+    };
+
+    indexed.onsuccess = function(event) {
+        var key = get_private_local()
+        const db = event.target.result;
+
+        var transaction = db.transaction(['keys'], 'readwrite');
+        var objectStore = transaction.objectStore('keys');
+        var keyData = { uid: auth.currentUser.uid, privateKey: key };
+        var request = objectStore.put(keyData);
+
+        request.onsuccess = function(event) {
+            console.log('Private key stored successfully');
+        };
+
+        request.onerror = function(event) {
+            console.error('Error storing private key');
+        };
+    }
+}
+
+
+
+/*
+setTimeout(() => {
+    //initialize()
+    
+    a = sodium.crypto_kx_keypair();
+    b = sodium.crypto_kx_keypair();
+
+    c = sodium.crypto_kx_client_session_keys(a.publicKey, a.privateKey, b.publicKey)
+    d = sodium.crypto_kx_server_session_keys(b.publicKey, b.privateKey, a.publicKey)
+
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+
+    message = "Hello, server!";
+    nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES);
+    ciphertext = sodium.crypto_secretbox_easy(message, nonce, c.sharedTx);
+    console.log("Encrypted message:", ciphertext);
+    console.log("Nonce:", nonce);
+
+    decryptedMessage = sodium.crypto_secretbox_open_easy(ciphertext, nonce, d.sharedRx);
+    console.log("Decrypted message:", sodium.to_string(decryptedMessage));
+
+    
+}, "1000");*/
