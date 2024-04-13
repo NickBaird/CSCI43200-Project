fab95de26ac110a1c3ada874f9d6b8f445545730
rp-bot_win10 <legacy.business18@gmail.com>
2024-04-13 11:29:00 -0400

    copy feature and other stuff

--- vue-firebase/src/components/MessagingCreators/GroupInvite.vue
+++ vue-firebase/src/components/MessagingCreators/GroupInvite.vue
@@ -3,7 +3,7 @@
     <h3 class="self-start text-xl font-semibold flex items-center gap-2">Invite to group <vue-feather type="user-plus"></vue-feather></h3>
    <input type="text" id="new-group-id" placeholder="Group ID" class="text-zinc-800 px-4 py-2"> 
    <input type="text" id="new-group-uid" class="text-zinc-800 px-4 py-2 " placeholder="User ID">
-    <button onclick="send_group_invite(document.getElementById('new-group-id').value, document.getElementById('new-group-uid').value)" class="bg-blue-500 hover:bg-blue-700 transition-colors  rounded-md px-4 py-2 flex items-center justify-center gap-2 text-[#EDF4ED]">Invite to Group</button>
+    <button onclick="send_group_invite(document.getElementById('new-group-id').value, document.getElementById('new-group-uid').value)" class="bg-blue-500 hover:bg-blue-700 transition-colors  rounded-md px-4 py-2 flex items-center justify-center gap-2 text-[#EDF4ED]">Invite to Group <vue-feather size="15" type="user-plus"></vue-feather></button>
 </div>
 </template>
 
--- vue-firebase/src/components/MessagingCreators/NewGroup.vue
+++ vue-firebase/src/components/MessagingCreators/NewGroup.vue
@@ -2,7 +2,7 @@
 <div id="new-group-container" class="flex flex-col items-center justify-between bg-[#db9b52] p-10  text-[#51291E] gap-6 z-0">
     <h3 class="self-start text-xl font-semibold flex items-center gap-2">Start a group chat <vue-feather type="users"></vue-feather></h3>
     <input type="text" id="new-group-name" class="text-zinc-800 px-4 py-2" placeholder="Group chat name"> 
-    <button onclick="new_group(document.getElementById('new-group-name').value);" class="bg-blue-500 hover:bg-blue-700 transition-colors  rounded-md px-4 py-2 flex items-center justify-center gap-2 text-[#EDF4ED]">Start Group <vue-feather type="plus-circle"></vue-feather></button>
+    <button onclick="new_group(document.getElementById('new-group-name').value);" class="bg-blue-500 hover:bg-blue-700 transition-colors  rounded-md px-4 py-2 flex items-center justify-center gap-2 text-[#EDF4ED]">Start Group <vue-feather size="15" type="users"></vue-feather></button>
 </div>
 </template>
 
