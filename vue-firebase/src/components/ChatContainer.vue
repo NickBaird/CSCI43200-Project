caaefe4e92f4b9be1a19c23ed49a1822f5b9f305
rp-bot_win10 <legacy.business18@gmail.com>
2024-04-17 20:11:29 -0400

    evins chanegs with styling -prat

---
+++ vue-firebase/src/components/ChatContainer.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="p-10 mb-20 flex flex-col  w-full gap-4">
+    <div v-for="messages in conversationLoad" class="flex flex-col w-full ">
+
+        <p :class="{ 
+            'bg-sky-300 p-4 self-end rounded-t-lg rounded-l-lg': (messages.me == 'yes'),
+            'bg-zinc-300 p-2 self-start rounded-t-lg rounded-r-lg': (messages.me != 'yes')
+        } ">{{ messages.message }}</p>
+    </div>
+</div>
+</template>
+
+<script>
+import { public_key } from '../../js.js';
+
+export default {
+    data() {
+        return {
+            sentMessages: [],
+            receivedMessages: [],
+            messages: [],
+            myKey: public_key
+        }
+    },
+    methods: {
+    },
+    computed: {
+    },
+    props: ['conversationLoad', 'update'],
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-firebase/src/components/MessageSender.vue
@@ -0,0 +1,67 @@
+<template>
+<div class="bg-pink-800 flex items-center gap-2 h-20 p-4" id="message-container" v-if="otherUID != (null || '')">
+    <button @click="reloadChat(responseData, otherUID)" class="flex items-center bg-green-400 h-full px-2 rounded-md"><vue-feather type="rotate-cw" class="text-white"></vue-feather></button>
+    <input type="text" id="message-box" v-model="message" class="w-1/2 h-1/2 outline-0 px-4 py-6 rounded-md" placeholder="Enter your message...">
+    <button @click="sendMessageVue(otherUID)" class="flex items-center bg-blue-500 h-full  rounded-md px-4"><vue-feather type="send" class="text-white"></vue-feather></button>
+    <input type='file' id='file-upload' class="hidden"/>
+    <label for="file-upload" class="flex items-center bg-yellow-500 h-full  rounded-md px-4 cursor-pointer"><vue-feather type="paperclip" class="text-white"></vue-feather></label>
+    <button @click="">Send File</button>
+</div>
+</template>
+
+<script>
+import {
+    send_message,
+    load_conversation
+} from '../../js.js';
+import VueFeather from "vue-feather";
+
+export default {
+    components: {
+        VueFeather
+    },
+    data() {
+        return {
+            message: '',
+            responseData: [],
+            newMessages: [],
+
+        }
+    },
+    methods: {
+        async sendMessageVue(uid) {
+            console.log("uid " + uid + " message " + this.message);
+            send_message(uid, this.message);
+
+            try {
+                this.responseData = await load_conversation(uid);
+                //let arr = [...this.responseData[0], ...this.responseData[1]]
+                //console.log(this.responseData);
+                //this.$emit('load-conversation', this.responseData);
+            } catch (e) {
+                console.log(e);
+            }
+
+            //this.reloadChat(this.responseData);
+        },
+        reloadChat(data, uid) {
+            if (data.length == 0) {} else {
+                console.log(data);
+                let sentMessages = data.sent;
+                sentMessages.forEach(obj => {
+                    obj.me = "yes";
+                });
+                let receivedMessages = data.received;
+                receivedMessages.forEach(obj => {
+                    obj.me = uid;
+                });
+                this.newMessages = sentMessages.concat(receivedMessages);
+                this.newMessages.sort((a, b) => a.timestamp - b.timestamp);
+                console.log(this.newMessages);
+                this.$emit('update-messages', this.newMessages);
+            }
+        }
+    },
+    props: ['otherUID'],
+}
+</script>
